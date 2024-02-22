fdff40872c0dc4b5ba2735bd47fc207fd04a751b
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2024-02-22 12:01:55 +0600

    final setup

--- src/Main/Home/Home.jsx
+++ src/Main/Home/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <div className="diff aspect-[16/9]">
-                    <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">Welcome To My Web Page
-                    </div>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="/src/assets/IMG_20210821_194310.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Hey Iam KB!</h1>
+                    <p className="py-6">You will find travel related videos on my channel.  You can join my YouTube channel, click on <span className='text-red-600'>"Get Started"</span> button to join my channel.  And yes don't forget to subscribe my channel.</p>
+                    <button className="btn btn-primary" ><a href="https://www.youtube.com/c/NewFutureBD">Get Started</a></button>
                 </div>
+            </div>
         </div>
     );
 };
--- src/Main/TravelVlogs/TravelVlogs.jsx
+++ src/Main/TravelVlogs/TravelVlogs.jsx
@@ -2,29 +2,75 @@ import React from 'react';
 
 const TravelVlogs = () => {
     return (
-        <>
-            <div className='ml-72 my-12'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/Yd6pmGXDG1g?si=wAt7KxevXbOWdBqD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div>
+            {/* w-16 md:w-32 lg:w-48 */}
+            <div >
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '
+                    height="315"
+                    src="https://www.youtube.com/embed/Yd6pmGXDG1g?si=wAt7KxevXbOWdBqD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/DuIte9EMihQ?si=k3f-W2YDU9FgvHAc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 ' height="315" src="https://www.youtube.com/embed/DuIte9EMihQ?si=k3f-W2YDU9FgvHAc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72 my-12'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ooJkEctDZrg?si=BW6G8mQTGJGlvHyJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '  height="315" src="https://www.youtube.com/embed/ooJkEctDZrg?si=BW6G8mQTGJGlvHyJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/7GC1La70OOQ?si=T0ukcVSLAv0BxDQN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 ' height="315" src="https://www.youtube.com/embed/7GC1La70OOQ?si=T0ukcVSLAv0BxDQN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72 my-12'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/xDVhwGmlp5w?si=80G62YdDoYLf9Lgi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '  height="315" src="https://www.youtube.com/embed/xDVhwGmlp5w?si=80G62YdDoYLf9Lgi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/goFVzTfaH-s?si=LGhTLj3ISoTgZEC-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '  height="315" src="https://www.youtube.com/embed/goFVzTfaH-s?si=LGhTLj3ISoTgZEC-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-            <div className='ml-72 my-12'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/r2YEpAe8UaY?si=TuhHHBwJ2eowWSfx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '  height="315" src="https://www.youtube.com/embed/r2YEpAe8UaY?si=TuhHHBwJ2eowWSfx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
-        </>
+            <div>
+                <iframe className='
+                    w-[440px] my-8
+                    lg:w-[560px] 
+                    lg:h-[315px] 
+                    lg:ml-72
+                    lg:my-10 '  height="315" src="https://www.youtube.com/embed/PYGY7OsFI9c?si=rqfqqREXulov2phx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
     );
 };
 
